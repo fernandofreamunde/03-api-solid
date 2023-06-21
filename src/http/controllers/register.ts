180572f9559be5866e42c02e0121bd37789c3bfb
Fernando Andrade <fernandofreamunde@gmail.com>
2023-06-21 15:39:44 +0200

    ✨ add user use case with patterns

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const userRepository = new PrismaUsersRepository()
+    const useCase = new RegisterUseCase(userRepository)
+
+    await useCase.execute({ name, email, password })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    throw err
+  }
+
+  return reply.status(201).send()
+}
---
+++ src/use-cases/register.ts
@@ -0,0 +1,30 @@
+/* eslint-disable camelcase */
+import { UsersRepository } from '@/repositories/user-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterUseCaseProps {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseProps) {
+    const password_hash = await hash(password, 8)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
